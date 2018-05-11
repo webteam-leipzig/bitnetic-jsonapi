a18319d1106fb8a741850a7e79543ca195690f9a
Mathias Lieber <lieber@bitnetic.de>
2018-05-12 00:10:11 +0200

    Fixed swagger doc

--- src/Resource/JsonApiResource.php
+++ src/Resource/JsonApiResource.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Resources\Json\Resource;
 
 /**
  * @SWG\Definition(
- *   required={"data, meta, errors"},
+ *   required={"data", "meta", "errors"},
  *   type="object",
  *   @SWG\Xml(name="JsonApiResource")
  * )
